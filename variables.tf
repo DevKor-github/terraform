ced229b91bfcc4a6c0938a1064fe7cf40ef949e2
BeLeap <beleap@beleap.dev>
2024-06-06 14:26:29 +0900

    fix: declare secured variable as sensitive

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "github_token" {
   type        = string
   description = "GitHub token"
+  sensitive = true
 }
 
 variable "discord_webhook_url" {
   description = "The Discord webhook URL to send notifications"
   type = string
+  sensitive = true
 }
